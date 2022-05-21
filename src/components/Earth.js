1451a24c413e07712779be813b5f91f96a30e0ea
Elias <id19epn@cs.umu.se>
2022-05-21 04:46:55 +0200

    asdfwa

--- src/components/Earth.js
+++ src/components/Earth.js
@@ -56,7 +56,7 @@ export default function Earth({ ...props }) {
     all.current.rotation.y += 0.001;
   });
 
-  const { nodes, materials } = useGLTF("ep-portfolio/earth.gltf");
+  const { nodes, materials } = useGLTF("/earth.gltf");
 
   materials.White = new MeshToonMaterial({ color: materials.White.color });
   materials.Snow = new MeshToonMaterial({ color: materials.Snow.color });
@@ -158,4 +158,4 @@ export default function Earth({ ...props }) {
   );
 }
 
-useGLTF.preload("ep-portfolio/earth.gltf");
+useGLTF.preload("/earth.gltf");
